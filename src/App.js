fa94f1f2ed8088eb7bf504ecc0f0b50927c681ab
Pankaj <udayrana428@gmail.com>
2021-10-16 18:49:42 +0530

    modified to function based component and refactor some bugs like sticky navbar, error while add page+1, etc.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,71 @@
+// import './App.css';
+
+// import React, { Component } from 'react'
+// import Navbar from './components/Navbar';
+// import News from './components/News';
+// import LoadingBar from 'react-top-loading-bar'
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   // Link
+// } from "react-router-dom";
+
+// export default class App extends Component {
+//   pageSize = 15
+//   country = "in"
+//   apikey=process.env.REACT_APP_API_KEY
+//   state = {
+//     progress: 20
+//   }
+//   setProgress = (progress) => {
+//     setState({ progress: progress })
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <Router>
+//           <Navbar />
+//           <LoadingBar
+//             height={3}
+//             color='#f11946'
+//             progress={state.progress}
+//           // onLoaderFinished={() => setProgress(0)}
+//           />
+//           <Switch>
+//             <Route exact path="/technology">
+//               <News apikey={apikey} setProgress={setProgress} key="technology" pageSize={pageSize} country={country} category="technology" />
+//             </Route>
+//             <Route exact path="/sports">
+//               <News apikey={apikey} setProgress={setProgress} key="sports" pageSize={pageSize} country={country} category="sports" />
+//             </Route>
+//             <Route exact path="/science">
+//               <News apikey={apikey} setProgress={setProgress} key="science" pageSize={pageSize} country={country} category="science" />
+//             </Route>
+//             <Route exact path="/health">
+//               <News apikey={apikey} setProgress={setProgress} key="health" pageSize={pageSize} country={country} category="health" />
+//             </Route>
+//             <Route exact path="/entertainment">
+//               <News apikey={apikey} setProgress={setProgress} key="entertainment" pageSize={pageSize} country={country} category="entertainment" />
+//             </Route>
+//             <Route exact path="/business">
+//               <News apikey={apikey} setProgress={setProgress} key="business" pageSize={pageSize} country={country} category="business" />
+//             </Route>
+//             {/* <Route exact path="/about">
+//               <News apikey={apikey} setProgress={setProgress} key="" pageSize={pageSize} country={country} category="business" />
+//             </Route> */}
+//             <Route exact path="/">
+//               <News apikey={apikey} setProgress={setProgress} key="general" pageSize={pageSize} country={country} category="general" />
+//             </Route>
+//           </Switch>
+//         </Router>
+//       </div>
+//     )
+//   }
+// }
 import './App.css';
 
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
 import LoadingBar from 'react-top-loading-bar'
@@ -11,55 +76,52 @@ import {
   // Link
 } from "react-router-dom";
 
-export default class App extends Component {
+export default function App(props) {
+  let { pageSize,country,apikey } = props
   pageSize = 15
   country = "in"
-  apikey=process.env.REACT_APP_API_KEY
-  state = {
-    progress: 20
+  apikey = process.env.REACT_APP_API_KEY
+  const [progress, setprogress] = useState(20)
+  const setProgress = (progress) => {
+    setprogress(progress)
   }
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
-  render() {
-    return (
-      <div>
-        <Router>
-          <Navbar />
-          <LoadingBar
-            height={3}
-            color='#f11946'
-            progress={this.state.progress}
-          // onLoaderFinished={() => setProgress(0)}
-          />
-          <Switch>
-            <Route exact path="/technology">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="technology" pageSize={this.pageSize} country={this.country} category="technology" />
-            </Route>
-            <Route exact path="/sports">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="sports" pageSize={this.pageSize} country={this.country} category="sports" />
-            </Route>
-            <Route exact path="/science">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="science" pageSize={this.pageSize} country={this.country} category="science" />
-            </Route>
-            <Route exact path="/health">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="health" pageSize={this.pageSize} country={this.country} category="health" />
-            </Route>
-            <Route exact path="/entertainment">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="entertainment" pageSize={this.pageSize} country={this.country} category="entertainment" />
-            </Route>
-            <Route exact path="/business">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="business" pageSize={this.pageSize} country={this.country} category="business" />
-            </Route>
-            {/* <Route exact path="/about">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="" pageSize={this.pageSize} country={this.country} category="business" />
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        // onLoaderFinished={() => setProgress(0)}
+        />
+        <Switch>
+          <Route exact path="/technology">
+            <News apikey={apikey} setProgress={setProgress} key="technology" pageSize={pageSize} country={country} category="technology" />
+          </Route>
+          <Route exact path="/sports">
+            <News apikey={apikey} setProgress={setProgress} key="sports" pageSize={pageSize} country={country} category="sports" />
+          </Route>
+          <Route exact path="/science">
+            <News apikey={apikey} setProgress={setProgress} key="science" pageSize={pageSize} country={country} category="science" />
+          </Route>
+          <Route exact path="/health">
+            <News apikey={apikey} setProgress={setProgress} key="health" pageSize={pageSize} country={country} category="health" />
+          </Route>
+          <Route exact path="/entertainment">
+            <News apikey={apikey} setProgress={setProgress} key="entertainment" pageSize={pageSize} country={country} category="entertainment" />
+          </Route>
+          <Route exact path="/business">
+            <News apikey={apikey} setProgress={setProgress} key="business" pageSize={pageSize} country={country} category="business" />
+          </Route>
+          {/* <Route exact path="/about">
+              <News apikey={apikey} setProgress={setProgress} key="" pageSize={pageSize} country={country} category="business" />
             </Route> */}
-            <Route exact path="/">
-              <News apikey={this.apikey} setProgress={this.setProgress} key="general" pageSize={this.pageSize} country={this.country} category="general" />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
-    )
-  }
+          <Route exact path="/">
+            <News apikey={apikey} setProgress={setProgress} key="general" pageSize={pageSize} country={country} category="general" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import loading from './loading.gif'
-export class Loading extends Component {
-    render() {
-        return (
-            <div className="text-center">
-                <img src={loading} alt="" />
-            </div>
-        )
-    }
-}
+// export class Loading extends Component {
+//     render() {
+//         return (
+//             <div className="text-center">
+//                 <img src={loading} alt="" />
+//             </div>
+//         )
+//     }
+// }
 
-export default Loading
+// export default Loading
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className="text-center">
+            <img src={loading} style={{height:"60px"}} alt="" />
+        </div>
+    )
+}
